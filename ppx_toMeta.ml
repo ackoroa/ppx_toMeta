b97f5c9415808602bcce5ad35d25360014ecd0a6
Arnold Koroa <ackoroa@comp.nus.edu.sg>
2015-03-01 14:40:24 +0800

    seperated rec call

--- ppx_toMeta.ml
+++ ppx_toMeta.ml
@@ -4,11 +4,6 @@ open Asttypes
 open Parsetree
 open Longident
 
-let isFunDef = fun structure_item ->
-  match structure_item with
-    {pstr_desc = Pstr_value (_,_)} -> true
-    | _ -> false
-
 let isRecursive = fun funDef ->
   match funDef with
     {pstr_desc = Pstr_value (Nonrecursive, _)} -> false
@@ -28,7 +23,7 @@ let getVars = fun funDef ->
          | _ -> []
   in aux pvb_expr
 
-let getAtrList = fun funDef ->
+let getAttrList = fun funDef ->
   match funDef with
     {pstr_desc = Pstr_value (_, value_binding_list)} ->
       (* value binding is a list for let .. and .. and .. *)
@@ -36,30 +31,64 @@ let getAtrList = fun funDef ->
       (List.hd value_binding_list).pvb_attributes
     | _ -> failwith "not a function definition"
 
-let rec extractStatVars = fun atrList ->
-  match atrList with
-    [] -> []
-    | ({txt = var}, _)::atrList -> var::(extractStatVars atrList)
-
 let getStatVars = fun funDef ->
-  let atrList = getAtrList funDef in
-  extractStatVars atrList
+  let attrList = getAttrList funDef in
+  let rec aux attrs =
+    match attrs with
+      [] -> []
+      | ({txt = var}, _)::attrs -> var::(aux attrs)
+  in aux attrList
 
-let getStagedName = fun origName vbLoc ->
-  match origName.ppat_desc with 
-    Ppat_var {loc = nameStrLoc; txt = origNameStr} ->
-        let nameStr = origNameStr^"S" in
-        Pat.var ~loc:vbLoc ~attrs:[] {loc=nameStrLoc;txt=nameStr}
-    | _ -> failwith "not a valid function name pattern"
+let rec removeArguments funBody n =
+  if n=0 
+    then funBody 
+    else 
+      match funBody with
+        {pexp_desc = Pexp_fun (_, _, _, funBody)} -> removeArguments funBody (n-1)
+        | _ -> failwith "arguments missing?"
+
+let subRecCall = fun funBody funName statVars ->
+  let rec sub funBody =
+    match funBody with
+      {pexp_desc = Pexp_ifthenelse (cond, thenExp, elseExpOpt); pexp_loc = loc} ->
+          begin match elseExpOpt with
+            None -> Exp.ifthenelse ~loc ~attrs:[] cond (sub thenExp) None
+            | Some elseExp -> Exp.ifthenelse ~loc ~attrs:[] cond (sub thenExp) (Some (sub elseExp))
+          end
+      | {pexp_desc = Pexp_apply (fn, argList); pexp_loc = loc} ->
+          let fname = 
+            begin match fn with 
+              {pexp_desc = Pexp_ident {txt = Lident fname}} -> fname
+              | _ -> failwith "not a valid function identifier"
+            end in
+          if fname = funName
+            then 
+              let fn' = Exp.ident ~loc ~attrs:[] {loc = loc; txt = Lident "f"} in
+              let argList' = 
+                let rec aux args =
+                  match args with
+                    [] -> []
+                    | (lbl, exp)::args -> 
+                        match exp with
+                          {pexp_desc = Pexp_ident {txt = Lident v}} ->
+                              if List.exists (fun sv -> sv=v) statVars
+                                then aux args
+                                else (lbl, exp)::(aux args)
+                          | exp -> (lbl, sub exp)::(aux args)
+                in aux argList
+              in Exp.apply ~loc ~attrs:[] fn' argList'
+            else Exp.apply ~loc ~attrs:[] fn argList
+      | exp -> exp
+  in sub funBody
 
 let buildArgList args body loc =
   let rec aux args =
     match args with
       [] -> body
       | arg::args -> 
-            Exp.fun_ ~loc ~attrs:[] "" None 
-              (Pat.var ~loc ~attrs:[] {loc=loc;txt=arg}) 
-              (aux args)
+          Exp.fun_ ~loc ~attrs:[] "" None 
+            (Pat.var ~loc ~attrs:[] {loc = loc; txt = arg}) 
+            (aux args)
   in aux args
 
 let buildStagedBody = fun statVars dynVars actualBody loc ->
@@ -75,22 +104,6 @@ let buildStagedBody = fun statVars dynVars actualBody loc ->
       [("", letBody)]
   in buildArgList statVars liftedLetBody loc
 
-let rec removeArguments funBody n =
-  if n=0 
-    then funBody 
-    else 
-      match funBody with
-        {pexp_desc = Pexp_fun (_, _, _, funBody)} -> removeArguments funBody (n-1)
-        | _ -> failwith "arguments missing?"
-
-let subRecCall = fun funBody funName statVars ->
-  (*let rec aux funBody =
-    match funBody with
-      {pexp_desc = Pexp_fun (lbl, opt, pat, funBody)} -> 
-          {pexp_desc = Pexp_fun (lbl, opt, pat, aux funBody)}
-      | _ -> failwith "not implemented"
-  in *)failwith "not implemented"
-
 let getStagedRecBody = fun origBody vbLoc vars statVars dynVars funName ->
   let nVars = List.length vars in
   let actualBody = removeArguments origBody nVars in
@@ -104,12 +117,16 @@ let getStagedBody = fun origBody vbLoc vars statVars dynVars ->
 
 let buildMeta = fun funRec funDef vars statVars dynVars -> 
   let strLoc = funDef.pstr_loc in
-  let f = match funDef.pstr_desc with
-            Pstr_value (_,f) -> f
-            | _ -> failwith "not a function definition"
+  let f = 
+    match funDef.pstr_desc with
+        Pstr_value (_, f) -> f
+        | _ -> failwith "not a function definition"
   in let vbLoc = (List.hd f).pvb_loc in
-  let funName = (List.hd f).pvb_pat in
-  let stagedName = getStagedName funName vbLoc in
+  let funName = 
+    match (List.hd f).pvb_pat with 
+      {ppat_desc = Ppat_var {txt = fn}} -> fn
+      | _ -> failwith "not a valid fun name pattern"
+  in let stagedName = Pat.var ~loc:vbLoc ~attrs:[] {loc = vbLoc; txt = (funName^"S")} in
   let funBody = (List.hd f).pvb_expr in
   if funRec
     then
@@ -122,15 +139,14 @@ let buildMeta = fun funRec funDef vars statVars dynVars ->
 let toMeta_mapper argv =
   { default_mapper with
     structure_item = fun mapper structure_item ->
-      if isFunDef structure_item 
-        then
+      match structure_item with
+        {pstr_desc = Pstr_value (_, _)} ->
           let r = isRecursive structure_item in
           let vars = getVars structure_item in
           let statVars = getStatVars structure_item in
           let dynVars = List.filter (fun v -> not (List.exists (fun sv -> v=sv) statVars)) vars in
           buildMeta r structure_item vars statVars dynVars
-        else
-          default_mapper.structure_item mapper structure_item
+        | _ -> default_mapper.structure_item mapper structure_item
   }
 
 let () = register "toMeta" toMeta_mapper
